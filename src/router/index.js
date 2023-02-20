6ed689e10df6ecb141a0b6ab37a4374be385530c
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-02-20 21:34:40 +0545

    Auth Scaffolding

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+function auth(to, from, next) {
+  if (!localStorage.getItem("access_token")) {
+    return next({ name: "register" });
+  }
+
+  next();
+}
+
+function guest(to, from, next) {
+  if (localStorage.getItem("access_token")) {
+    return next({ name: "vehicles.index" });
+  }
+
+  next();
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       name: "home",
-      component: import("../views/HomeView.vue"),
+      component: import("@/views/HomeView.vue"),
     },
     {
       path: "/register",
       name: "register",
+      beforeEnter: guest,
       component: () => import("@/views/Auth/RegisterView.vue"),
+    },
+    {
+      path: "/vehicles",
+      name: "vehicles.index",
+      beforeEnter: auth,
+      component: () => import("@/views/Vehicles/IndexView.vue"),
     },
   ],
 });
